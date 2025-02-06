12a6b645d925cdbe0ff17a3ab346c221316bff94
N41G3 <nnaigeon@outlook.fr>
2025-02-06 12:40:23 +0100

    factorize the code

--- booking-up-for-beauty/BookingUpForBeauty.cs
+++ booking-up-for-beauty/BookingUpForBeauty.cs
@@ -6,14 +6,11 @@ static class Appointment
     
     public static bool HasPassed(DateTime appointmentDate)=>DateTime.Now > appointmentDate;
     
-    public static bool IsAfternoonAppointment(DateTime appointmentDate)=>appointmentDate.Hour>=12 && 18>appointmentDate.Hour;
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)=>appointmentDate.Hour >= 12 && 18 > appointmentDate.Hour;
 
-    public static string Description(DateTime appointmentDate)=>"You have an appointment on "+appointmentDate+".";
+    public static string Description(DateTime appointmentDate) =>$"You have an appointment on {appointmentDate}.";
    
 
-    public static DateTime AnniversaryDate()
-    {
-        DateTime birthday = new DateTime(DateTime.Now.Year, 9, 15,0,0,0);  
-        return birthday;
-    }
+    public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15,0,0,0);
+   
 }
