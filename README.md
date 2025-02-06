6f470f85c2eaa1402321a1cc406116b46632133a
N41G3 <nnaigeon@outlook.fr>
2025-02-06 08:54:05 +0100

    README + booking-up-for-beauty + log-analysis

---
+++ README.md
@@ -0,0 +1,31 @@
+# CSharp
+
+Welcome to C#(CSharp) homework Track.
+
+## Introduction
+This is a simple C# folder with my homework to proof my skill to myself ☺♥
+
+## What's here ?
+some folder to learn C# lang 
+- annalyns-infiltration
+- booking-up-for-beauty
+- cars-assemble
+- hello-world ->The first one ♥
+- log-analysis
+- log-levels
+- lucians-luscious-lasagna
+- phone-number-analysis
+
+
+
+## Source
+- @N4193
+
+### Special thank's to ppl creat exercise for me to solve 
+//sorry for ppl i forget here i love you a lot ♥
+- @yzAlvin
+- @ErikSchierboom
+- @valentin-p
+- @mikedamay
+- @j2jensen
+- @robkeim
--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,12 +1,18 @@
 using System;
 
 public static class LogAnalysis 
-{
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
-
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
+{  
+    public static string SubstringAfter(this string log,string delimiteur) => log.Split(delimiteur)[1];
+    public static string SubstringBetween(this string message, string firstDelimeter, string secondDelimeter)=> message.Split(firstDelimeter)[1].Split(secondDelimeter)[0];
     
     // TODO: define the 'Message()' extension method on the `string` type
-
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string Message(this string log)
+    {
+        return log;
+    }
+    // TODO: define the 'LogLevel()' extension method on the `string` type 
+    public static string LogLevel(this string log)
+    {
+        return log;
+    }
 }
