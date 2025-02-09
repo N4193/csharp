fd76d870962197ba7fbd26c4ebace2a2621c6ed4
N41G3 <nnaigeon@outlook.fr>
2025-02-09 13:36:45 +0100

    solve the problem of the log analysis

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,18 +1,13 @@
 using System;
 
 public static class LogAnalysis 
-{  
-    public static string SubstringAfter(this string log,string delimiteur) => log.Split(delimiteur)[1];
-    public static string SubstringBetween(this string message, string firstDelimeter, string secondDelimeter)=> message.Split(firstDelimeter)[1].Split(secondDelimeter)[0];
+{
+    // TODO: define the 'SubstringAfter()' extension method on the `string` type   
+    public static string SubstringAfter(this string log,string delimiteur) => log.Split(delimiteur)[1]; 
+
+    public static string SubstringBetween(this string message, string firstDelimeter, string secondDelimeter) => message.Split(firstDelimeter)[1].Split(secondDelimeter)[0];
+
+    public static string Message(this string log) => log.SubstringAfter(": ");
     
-    // TODO: define the 'Message()' extension method on the `string` type
-    public static string Message(this string log)
-    {
-        return log;
-    }
-    // TODO: define the 'LogLevel()' extension method on the `string` type 
-    public static string LogLevel(this string log)
-    {
-        return log;
-    }
+    public static string LogLevel(this string log) => log.SubstringBetween("[", "]");
 }
