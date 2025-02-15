1a0a09df2e77dbb4cc02c44aff38c9e1539c81b2
N41G3 <nnaigeon@outlook.fr>
2025-02-15 13:11:29 +0100

    update solved : leap factored v2.1

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -1,16 +1,5 @@
 using System;
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        bool _IsLeapYear = false; //init the return variable
-        
-        if ((((year % 4) == 0 ) && ((year % 400) == 0) && ((year % 100) == 0)) || (((year % 100) != 0) && ((year % 4) == 0)))
-        {       
-            //Console.WriteLine($"{year} is divisible by 4 & 400 & 100 !");
-            //Console.WriteLine($"{year} is leap year !");
-            _IsLeapYear = true;
-        }
-        return _IsLeapYear;
-    }
+    public static bool IsLeapYear(int year) => ((((year % 4) == 0 ) && ((year % 400) == 0) && ((year % 100) == 0)) || (((year % 100) != 0) && ((year % 4) == 0)));
 }
