ce7283d60017027c396a2cfe0557b317f2c1d5d3
N41G3 <nnaigeon@outlook.fr>
2025-02-15 13:15:35 +0100

    update solved : leap v3.5 max-FACTORED

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -1,17 +1,5 @@
 using System;
 public static class Leap
 {
-    public static bool IsLeapYear(int year) 
-    {
-        bool _IsLeapYear = false;
-        if ((year % 4) == 0 ) 
-        {
-            _IsLeapYear = true;
-            if (((year % 100) == 0) && ((year % 400) != 0 ))
-            {
-                _IsLeapYear = false;
-            }
-        } 
-        return _IsLeapYear;
-    } 
+    public static bool IsLeapYear(int year) => (((year % 4) == 0 )) ? ((((year % 100) == 0) && ((year % 400) != 0 )) ? false : true) : false;
 }
