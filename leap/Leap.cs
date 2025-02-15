128af0a516d483412533f0cd8dcd4f813426f5e1
N41G3 <nnaigeon@outlook.fr>
2025-02-15 13:14:05 +0100

    update solved : leap v3 (logic :only 3 check !)

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -1,5 +1,17 @@
 using System;
 public static class Leap
 {
-    public static bool IsLeapYear(int year) => ((((year % 4) == 0 ) && ((year % 400) == 0) && ((year % 100) == 0)) || (((year % 100) != 0) && ((year % 4) == 0)));
+    public static bool IsLeapYear(int year) 
+    {
+        bool _IsLeapYear = false;
+        if ((year % 4) == 0 ) 
+        {
+            _IsLeapYear = true;
+            if (((year % 100) == 0) && ((year % 400) != 0 ))
+            {
+                _IsLeapYear = false;
+            }
+        } 
+        return _IsLeapYear;
+    } 
 }
