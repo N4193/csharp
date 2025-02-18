6abdea02d40e69015ba3c4936cbb8598541566fa
N41G3 <nnaigeon@outlook.fr>
2025-02-18 17:03:14 +0100

    update solved : SecureMunchesterUnited V1.0

--- secure-munchester-united/SecureMunchesterUnited.cs
+++ secure-munchester-united/SecureMunchesterUnited.cs
@@ -4,7 +4,16 @@ public class SecurityPassMaker
 {
     public string GetDisplayName(TeamSupport support)
     {
-        throw new NotImplementedException($"Please implement the SecurityPassMaker.GetDisplayName() method");
+        if (support is Manager){return $"Too Important for a Security Pass";} 
+        if ((support is Staff) && !(support is Security)){return $"{support.Title}";}
+        if ((support is Security &&
+             (support.GetType()).ToString() != "SecurityJunior" &&
+             (support.GetType()).ToString() != "SecurityIntern" &&
+             (support.GetType()).ToString() != "PoliceLiaison" ))
+            {
+                return $"{support.Title} Priority Personnel";
+            }
+        return support.Title;      
     }
 }
 
@@ -12,22 +21,22 @@ public class SecurityPassMaker
 
 public interface TeamSupport { string Title { get; } }
 
-public abstract class Staff : TeamSupport { public abstract string Title { get; } }
+    public abstract class Staff : TeamSupport { public abstract string Title { get; } }
 
-public class Manager : TeamSupport { public string Title { get; } = "The Manager"; }
+    public class Manager : TeamSupport { public string Title { get; } = "The Manager"; }
 
-public class Chairman : TeamSupport { public string Title { get; } = "The Chairman"; }
+    public class Chairman : TeamSupport { public string Title { get; } = "The Chairman"; }
 
-public class Physio : Staff { public override string Title { get; } = "The Physio"; }
+            public class Physio : Staff { public override string Title { get; } = "The Physio"; }
 
-public class OffensiveCoach : Staff { public override string Title { get; } = "Offensive Coach"; }
+            public class OffensiveCoach : Staff { public override string Title { get; } = "Offensive Coach"; }
 
-public class GoalKeepingCoach : Staff { public override string Title { get; } = "Goal Keeping Coach"; }
+            public class GoalKeepingCoach : Staff { public override string Title { get; } = "Goal Keeping Coach"; }
 
-public class Security : Staff { public override string Title { get; } = "Security Team Member"; }
+            public class Security : Staff { public override string Title { get; } = "Security Team Member"; }
 
-public class SecurityJunior : Security { public override string Title { get; } = "Security Junior"; }
+                public class SecurityJunior : Security { public override string Title { get; } = "Security Junior"; }
 
-public class SecurityIntern : Security { public override string Title { get; } = "Security Intern"; }
+                public class SecurityIntern : Security { public override string Title { get; } = "Security Intern"; }
 
-public class PoliceLiaison : Security { public override string Title { get; } = "Police Liaison Officer"; }
+                public class PoliceLiaison : Security { public override string Title { get; } = "Police Liaison Officer"; }
