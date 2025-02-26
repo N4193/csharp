320e87580c2ec3eac60e984a464b9ca4084699e8
N41G3 <nnaigeon@outlook.fr>
2025-02-26 01:19:42 +0100

    update AttackOfTheTrolls.cs + Factor

--- attack-of-the-trolls/AttackOfTheTrolls.cs
+++ attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -1,26 +1,43 @@
-// TODO: define the 'AccountType' enum
+using System;
+using System.Reflection;
 
-// TODO: define the 'Permission' enum
+[Flags]
+enum AccountType : byte
+{
+    Guest = 0b_0010_0001,//1
+    User = 0b_0100_0011,//1+2=3
+    Moderator = 0b_1000_0111//1+2+4=7
+}
 
+[Flags]
+enum Permission : byte
+{
+    None = 0b_0000_0000,
+    Read = 0b_0000_0001,
+    Write = 0b_0000_0010,
+    Delete = 0b_0000_0100,
+    All = 0b_0000_0111
+}
+/*
+     0 = None           => 0
+     1 = Read           => 1
+     2 = Write          => 2
+     3 = Read, Write    => 2+1=3
+     4 = Delete         => 4
+     5 = Read, Delete   => 4+1=5
+     6 = Write, Delete  => 4+2=6
+     7 = All            => 4+2+1=7
+*/
 static class Permissions
 {
-    public static Permission Default(AccountType accountType)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Default() method");
-    }
-
-    public static Permission Grant(Permission current, Permission grant)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Grant() method");
-    }
-
-    public static Permission Revoke(Permission current, Permission revoke)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Revoke() method");
-    }
-
-    public static bool Check(Permission current, Permission check)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Check() method");
-    }
+    public static Permission Default(AccountType accountType) => accountType switch
+        {
+            AccountType.Guest => (Permission)1,
+            AccountType.User => (Permission)3,
+            AccountType.Moderator => (Permission)7,
+            _ => (Permission)0
+        };
+    public static Permission Grant(Permission current, Permission grant) => current | grant;
+    public static Permission Revoke(Permission current, Permission revoke) => current & ~revoke;
+    public static bool Check(Permission current, Permission check) => (current & check)==check ? true : false; 
 }
