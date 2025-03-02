acb2d1ada1e5b70314b7289ea05c76cf1cf31c34
N4193 <nnaigeon@outlook.fr>
2025-03-02 15:22:31 +0100

    update InternationalCallingConnoisseur finish

--- international-calling-connoisseur/InternationalCallingConnoisseur.cs
+++ international-calling-connoisseur/InternationalCallingConnoisseur.cs
@@ -1,51 +1,44 @@
 public static class DialingCodes
 {
-    public static Dictionary<int, string> GetEmptyDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetEmptyDictionary() method");
-    }
-
-    public static Dictionary<int, string> GetExistingDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetExistingDictionary() method");
-    }
-
+    public static Dictionary<int, string> GetEmptyDictionary()=> new Dictionary<int, string>();
+    public static Dictionary<int, string> GetExistingDictionary()=> new Dictionary<int, string>
+        {
+            [1] = "United States of America",
+            [55] = "Brazil",
+            [91] = "India" 
+        };
     public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
     {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToEmptyDictionary() method");
+        Dictionary<int, string> myDictionary = GetEmptyDictionary();
+        myDictionary.Add(countryCode, countryName);
+        return myDictionary;
     }
-
-    public static Dictionary<int, string> AddCountryToExistingDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+    public static Dictionary<int, string> AddCountryToExistingDictionary(Dictionary<int, string> existingDictionary, int countryCode, string countryName)
     {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToExistingDictionary() method");
+         existingDictionary.Add(countryCode, countryName);
+         return existingDictionary;
     }
-
-    public static string GetCountryNameFromDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode)
+    public static string GetCountryNameFromDictionary(Dictionary<int, string> existingDictionary, int countryCode)=> existingDictionary.ContainsKey(countryCode)?existingDictionary[countryCode]:string.Empty;
+    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)=>existingDictionary.ContainsKey(countryCode);
+    public static Dictionary<int, string> UpdateDictionary(Dictionary<int, string> existingDictionary, int countryCode, string countryName)
     {
-        throw new NotImplementedException($"Please implement the (static) GetCountryNameFromDictionary() method");
+        if(CheckCodeExists(existingDictionary,countryCode))
+            existingDictionary[countryCode]=countryName;
+        return existingDictionary;
     }
-
-    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
+    public static Dictionary<int, string> RemoveCountryFromDictionary(Dictionary<int, string> existingDictionary, int countryCode)
     {
-        throw new NotImplementedException($"Please implement the (static) CheckCodeExists() method");
+        existingDictionary.Remove(countryCode);
+        return existingDictionary;
     }
-
-    public static Dictionary<int, string> UpdateDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
-    }
-
-    public static Dictionary<int, string> RemoveCountryFromDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
-    }
-
     public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
     {
-        throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
+        string LongestCountryName = "";
+        foreach( KeyValuePair<int, string> kvp in existingDictionary )
+        {
+            if (kvp.Value.Length > LongestCountryName.Length)
+                LongestCountryName = kvp.Value;
+        }
+        return LongestCountryName;
     }
 }
