46169214fa93b2bd9d9b56cfd5e11f3c6d247589
N4193 <nnaigeon@outlook.fr>
2025-03-26 18:28:55 +0100

    update Resistor-Color exercices finish

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -1,12 +1,27 @@
+using System;
+using System.Reflection;
+
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
+    public static int ColorCode(string color) => color switch
     {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+        "black" => 0,
+        "brown"=> 1,
+        "red"=> 2,
+        "orange"=> 3,
+        "yellow"=> 4,
+        "green"=> 5,
+        "blue"=> 6,
+        "violet"=> 7,
+        "grey"=> 8,
+        "white"=> 9,
+        _ => 0
+    };
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        string[] color = new string[10];
+        color = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+        return color;
     }
 }
